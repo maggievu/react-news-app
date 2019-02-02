b0fb8364c921aab9bd84333461f598e3e4f78539
Maggie Vu <maggievu91@gmail.com>
2019-02-02 03:22:45 -0800

    Updates

---
+++ precache-manifest.f4241e8e715e1f418d8b155206ed5bb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0914b53e62f7f4a97c4d",
+    "url": "/react-news-app/static/js/runtime~main.0914b53e.js"
+  },
+  {
+    "revision": "f61cdd3bed7d166daa20",
+    "url": "/react-news-app/static/js/main.f61cdd3b.chunk.js"
+  },
+  {
+    "revision": "3efb07aabf137b777986",
+    "url": "/react-news-app/static/js/1.3efb07aa.chunk.js"
+  },
+  {
+    "revision": "f61cdd3bed7d166daa20",
+    "url": "/react-news-app/static/css/main.308439bd.chunk.css"
+  },
+  {
+    "revision": "9cba1dce9d7a983533d43330779947a8",
+    "url": "/react-news-app/index.html"
+  }
+];
