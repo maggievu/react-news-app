4b572168f1491ded20616fa103cd1f2f38f9f928
Maggie Vu <maggievu91@gmail.com>
2019-02-03 05:04:10 -0800

    Updates

--- precache-manifest.757760caab18c1fb61046d806d046c20.js
+++ precache-manifest.757760caab18c1fb61046d806d046c20.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-news-app/static/js/runtime~main.0914b53e.js"
   },
   {
-    "revision": "6a0fee05f48f48cd2e19",
-    "url": "/react-news-app/static/js/main.6a0fee05.chunk.js"
+    "revision": "b4d7b10aeadbe3b45983",
+    "url": "/react-news-app/static/js/main.b4d7b10a.chunk.js"
   },
   {
     "revision": "ad581cde1dbd7095c96a",
     "url": "/react-news-app/static/js/1.ad581cde.chunk.js"
   },
   {
-    "revision": "6a0fee05f48f48cd2e19",
+    "revision": "b4d7b10aeadbe3b45983",
     "url": "/react-news-app/static/css/main.b68eba9c.chunk.css"
   },
   {
-    "revision": "7d5579898e5b94add25919cc0f231754",
+    "revision": "a42c3b5ed69f97e542efcd4ee1912614",
     "url": "/react-news-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-news-app/precache-manifest.4c7f30eb1bf4e92ab1bb294917af1a8f.js"
+  "/react-news-app/precache-manifest.757760caab18c1fb61046d806d046c20.js"
 );
 
 workbox.clientsClaim();
