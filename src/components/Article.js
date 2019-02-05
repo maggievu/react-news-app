fa6a6165c1763a36010dae514ed0f79e89075578
Maggie Vu <maggievu91@gmail.com>
2019-02-05 14:22:33 -0800

    apply css rule inside JSS (smarter way to style)

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,32 +1,44 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = {
+  root: {
+    width: '96%',
+    margin: '2rem 2%',
+    backgroundColor: '#fff',
+  },
+  title: {
+    marginTop: 0,
+  }
+}
 
 const Article = (props) => (
-<Grid className='article' container spacing={32}>
-  <Grid item md={4}>
-    <a href={props.url} className='article_key'>
-      {props.urlToImage && <img src={props.urlToImage} alt={props.title} />}
-      {!props.urlToImage && <p>Source</p>}
-    </a>
-  </Grid>
+  <Grid className={props.classes.root} container spacing={32}>
+    <Grid item md={4}>
+      <a href={props.url}>
+        {props.urlToImage && <img src={props.urlToImage} alt={props.title} />}
+        {!props.urlToImage && <p>Source</p>}
+      </a>
+    </Grid>
 
-  <Grid item md={8}>
-    <h3 className='article_key'>{props.title}</h3>
-    <p className='article_key'>
-        <em>
-          {props.author !== null && `By ${props.author}. `}
-          From <a href={props.url}>{props.source}</a>. Published at {props.publishedAt}
-        </em>
-    </p>
-    <p className='article_key'>
-      {props.content}
-    </p>
-    <p className='article_key'>
-        <em>{props.description !== '' && `Description: ${props.description}`}</em>
-    </p>
-  </Grid>
+    <Grid item md={8}>
+      <h3 className={props.classes.title}>{props.title}</h3>
+      <p>
+          <em>
+            {props.author !== null && `By ${props.author}. `}
+            From <a href={props.url}>{props.source}</a>. Published at {props.publishedAt}
+          </em>
+      </p>
+      <p>
+        {props.content}
+      </p>
+      <p>
+          <em>{props.description !== '' && `Description: ${props.description}`}</em>
+      </p>
+    </Grid>
 
-</Grid>
+  </Grid>
 )
 
-export default Article
+export default withStyles(styles)(Article)
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import { withStyles } from '@material-ui/core/styles'
 import BottomNavigation from '@material-ui/core/BottomNavigation'
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
 
-const styles = () => ({
+const styles = (theme) => ({
   root: {
     width: '100%',
     height: 'auto',
@@ -14,12 +14,30 @@ const styles = () => ({
     backgroundColor: 'rgba(0,0,0,.1)',
     border: '1px solid #fff',
     flex: '1 0 120px',
+    [theme.breakpoints.down('709')]: {
+      flexBasis: '50%',
+    },
+    maxWidth: 'initial',
+    textTransform: 'uppercase',
+  },
+  searchResultRoot: {
+    backgroundColor: 'rgba(0,0,0,.1)',
+    border: '1px solid #fff',
+    flex: '1 0 120px',
+    [theme.breakpoints.down('709')]: {
+      order: -1,
+      flexBasis: '100%',
+    },
     maxWidth: 'initial',
     textTransform: 'uppercase',
   },
   label: {
     padding: '1rem 0',
     fontSize: '14px',
+    [theme.breakpoints.down('709')]: {
+      paddingTop: '.5rem',
+      paddingBottom: '.5rem',
+    },
   },
   selected: {
     color: '#fff',
@@ -49,7 +67,7 @@ class Navigation extends Component {
       >
         <BottomNavigationAction label="USA" value="us" classes={{ root: this.props.classes.actionRoot, label: this.props.classes.label, selected: this.props.classes.selected }} />
         <BottomNavigationAction label="Canada" value="ca" classes={{ root: this.props.classes.actionRoot, label: this.props.classes.label, selected: this.props.classes.selected }} />
-        <BottomNavigationAction label="Search Results" value="" classes={{ root: this.props.classes.actionRoot, label: this.props.classes.label, selected: this.props.classes.selected }} />
+        <BottomNavigationAction label="Search Results" value="" classes={{ root: this.props.classes.searchResultRoot, label: this.props.classes.label, selected: this.props.classes.selected }} />
         <BottomNavigationAction label="France" value="fr" classes={{ root: this.props.classes.actionRoot, label: this.props.classes.label, selected: this.props.classes.selected }} />
         <BottomNavigationAction label="Belgium" value="be" classes={{ root: this.props.classes.actionRoot, label: this.props.classes.label, selected: this.props.classes.selected }} />
       </BottomNavigation>
